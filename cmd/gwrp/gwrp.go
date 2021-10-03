2ec207f3beae2fa731f5dae38304acd48197503d
Stewart Platt <shteou@gmail.com>
2021-10-03 18:39:09 +0100

    feat: graceful shutdown
    Add a basic graceful shutdown handler which responds to SIGINT and
    SIGTERM (for local dev + deployment to, e.g., k8s)

--- cmd/gwrp/gwrp.go
+++ cmd/gwrp/gwrp.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
 
 	"github.com/shteou/gwrp/pkg/handlers"
 )
+
+type server struct {
+	http.Server
+	reqCount uint32
+}
+
+func (s *server) waitShutdown(channel chan os.Signal) {
+	// listen for SiGINT (ctrl+c) and SIGTERM
+	signal.Notify(channel, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-channel
+	fmt.Printf("Received interrupt (signal: %v)\n", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err := s.Shutdown(ctx)
+	if err != nil {
+		fmt.Printf("Failed to shutdown: %v\n", err)
+	}
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -18,11 +44,24 @@ func main() {
 
 	http.Handle("/", r)
 
-	srv := &http.Server{
-		Handler: r,
-		Addr:    "0.0.0.0:8080",
-		WriteTimeout: 60 * time.Second,
-		ReadTimeout:  60 * time.Second,
+	srv := server{
+		Server: http.Server{
+			Handler:      r,
+			Addr:         "0.0.0.0:8080",
+			WriteTimeout: 60 * time.Second,
+			ReadTimeout:  60 * time.Second,
+		},
 	}
-	srv.ListenAndServe()
+
+	done := make(chan os.Signal, 1)
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			fmt.Printf("Listen and serve: %v\n", err)
+		}
+		// Emulate shutdown interrupt
+		done <- syscall.SIGINT
+	}()
+
+	srv.waitShutdown(done)
 }
